71f80490210493194cc69e64562dd67d14875814
AliaghaSahibcanov <95203241+xalien666@users.noreply.github.com>
2022-09-20 21:46:35 +0400

    Add files via upload

---
+++ Loop/Practice4.py
@@ -0,0 +1,38 @@
+print(
+      """
+      Bankomat
+      
+      Proqramdan cixmaq istedikde "q" ya basin
+      
+      Proses:
+      1-Pul sorgula
+      2-Pul yatirma
+      3-Pul cekme
+      """
+      )
+hesabdakipul = 1000
+while True:
+    proses = input("Prosesi daxil edin")
+    
+    if (proses =="q"):
+        print("Proqramdan cixilir")
+        break
+    
+    elif (proses == "1"):
+        print(hesabdakipul)
+    
+    elif (proses=="2"):
+        mebleg = int(input("Yatirmag istediyiniz meblegi girin"))
+    
+        hesabdakipul = hesabdakipul + mebleg
+        print(hesabdakipul)
+        
+    elif (proses=="3"):
+        mebleg = int(input("Cekmek istediyiniz meblegi girin"))
+        hesabdakipul=hesabdakipul-mebleg
+        if hesabdakipul<0:
+            print("Hesabinizda yeterli mebleg yoxdur")
+            hesabdakipul=hesabdakipul+mebleg
+       
+    else:
+        print("Prosesi duzgun girin")
